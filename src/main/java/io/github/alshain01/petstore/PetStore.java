40c444f72e9b47e184c1197e4e930af2fd7f052d
Kevin Seiden <alshain01@suddenlink.net>
2014-02-04 19:09:01 -0600

    Add metrics & updater

--- src/main/java/io/github/alshain01/petstore/PetStore.java
+++ src/main/java/io/github/alshain01/petstore/PetStore.java
@@ -11,15 +11,20 @@ import org.bukkit.entity.Tameable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import io.github.alshain01.petstore.Updater.UpdateResult;
+
+import java.io.IOException;
 import java.util.*;
 
 public class PetStore extends JavaPlugin {
-    public final static long timeout = 250;
+    public static long timeout;
     public final static ChatColor errorColor = ChatColor.RED;
     public final static ChatColor warnColor = ChatColor.YELLOW;
     public final static ChatColor notifyColor = ChatColor.BLUE;
@@ -28,12 +33,15 @@ public class PetStore extends JavaPlugin {
     private TransferAnimal transfer = new TransferAnimal();
     private GiveAnimal give = null;
     private SellAnimal sales = null;
+    private Updater updater = null;
 
     private Set<UUID> cancelQueue = new HashSet<UUID>();
 
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
         CustomYML yml = new CustomYML(this, "data.yml");
+        timeout = this.getConfig().getLong("CommandTimeout");
 
         // Read give aways from file
         give = new GiveAnimal(yml.getConfig().getList("Give", new ArrayList<String>()));
@@ -64,6 +72,20 @@ public class PetStore extends JavaPlugin {
             }
 
             pm.registerEvents(sales, this);
+        }
+/*
+        if (getConfig().getBoolean("Update.Check")) {
+            new UpdateScheduler().runTaskTimer(this, 0, 1728000);
+            getServer().getPluginManager().registerEvents(new UpdateListener(), this);
+        }
+*/
+
+        if(this.getConfig().getBoolean("Metrics.Enabled")) {
+            try {
+                new MetricsLite(this).start();
+            } catch (IOException ex) {
+                this.getLogger().warning("Metrics failed to start.");
+            }
         }
     }
 
@@ -175,6 +197,53 @@ public class PetStore extends JavaPlugin {
                     sales.cancel(player, entity);
                 }
                 cancelQueue.remove(e.getPlayer().getUniqueId());
+            }
+        }
+    }
+
+    /*
+     * Contains event listeners required for plugin maintenance.
+     */
+    private class UpdateListener implements Listener {
+        // Update listener
+        @EventHandler(ignoreCancelled = true)
+        private void onPlayerJoin(PlayerJoinEvent e) {
+            if(updater == null) { return; }
+            if (e.getPlayer().hasPermission("petstore.admin.notifyupdate")) {
+                if(updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE) {
+                    e.getPlayer().sendMessage(ChatColor.DARK_PURPLE
+                            + "The version of PetStore that this server is running is out of date. "
+                            + "Please consider updating to the latest version at dev.bukkit.org/bukkit-plugins/petstore/.");
+                } else if(updater.getResult() == UpdateResult.SUCCESS) {
+                    e.getPlayer().sendMessage("[PetStore] " + ChatColor.DARK_PURPLE
+                            + "An update to PetStore has been downloaded and will be installed when the server is reloaded.");
+                }
+            }
+        }
+    }
+
+    /*
+     * Handles update checking and downloading
+     */
+    private class UpdateScheduler extends BukkitRunnable {
+        @Override
+        public void run() {
+            // Update script
+            final String key = getConfig().getString("Update.ServerModsAPIKey");
+            final Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("PetStore");
+            updater = (getConfig().getBoolean("Update.Download"))
+                    ? new Updater(plugin, 0, getFile(), Updater.UpdateType.DEFAULT, key, true)
+                    : new Updater(plugin, 0, getFile(), Updater.UpdateType.NO_DOWNLOAD, key, false);
+
+            if (updater.getResult() == UpdateResult.UPDATE_AVAILABLE) {
+                Bukkit.getServer().getConsoleSender()
+                        .sendMessage("[PetStore] "	+ ChatColor.DARK_PURPLE
+                                + "The version of PetStore that this server is running is out of date. "
+                                + "Please consider updating to the latest version at dev.bukkit.org/bukkit-plugins/petstore/.");
+            } else if (updater.getResult() == UpdateResult.SUCCESS) {
+                Bukkit.getServer().getConsoleSender()
+                        .sendMessage("[PetStore] "	+ ChatColor.DARK_PURPLE
+                                + "An update to PetStore has been downloaded and will be installed when the server is reloaded.");
             }
         }
     }
